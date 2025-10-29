7fc90ebe7ae32ff963dea8491510ad5fc69ae06a
Radja <radjaguemoula@gmail.com>
2025-10-29 23:48:55 +0100

    refactoring

--- src/string_calculater.py
+++ src/string_calculater.py
@@ -1,14 +1,8 @@
 def string_calculater(n:str)-> int:
     if n == '':
       return 0
-    if n == '0,0':
-      return 0
-    if n == '0,1':
-      return 1
-    if n == '1,0':
-        return 1
-    if n == '1,1':
-        return 2
-    if n == '1,2':
-        return 3
-    return int(n)
+    parts = n.split(',')
+    total = 0
+    for num in parts:
+        total += int(num)
+    return total
