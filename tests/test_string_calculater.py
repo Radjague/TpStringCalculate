01a8f842e7bfdaaa2aeb487b8911ed3150cf85cc
Radja <radjaguemoula@gmail.com>
2025-10-29 22:40:27 +0100

    refactoring

--- tests/test_string_calculater.py
+++ tests/test_string_calculater.py
@@ -1,8 +1,5 @@
 from src.string_calculater import string_calculater
 
 
-def test_string_calculater_function_is_callable():
-    string_calculater('')
-
 def test_empty_string_return_0():
     assert string_calculater('') == 0
