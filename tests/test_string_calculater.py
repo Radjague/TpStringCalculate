5b9fa26861887f627ac01c24c8f2a4b193c21b70
Radja <radjaguemoula@gmail.com>
2025-10-29 22:43:29 +0100

    test 0 return 0

--- tests/test_string_calculater.py
+++ tests/test_string_calculater.py
@@ -3,3 +3,6 @@ from src.string_calculater import string_calculater
 
 def test_empty_string_return_0():
     assert string_calculater('') == 0
+
+def test_0_return_0():
+    assert string_calculater('0') == 0
