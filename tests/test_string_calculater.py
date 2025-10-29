a8f5845f5370e894d943b1d7873796b553686656
Radja <radjaguemoula@gmail.com>
2025-10-29 23:57:27 +0100

    Other tests ..

--- tests/test_string_calculater.py
+++ tests/test_string_calculater.py
@@ -1,7 +1,7 @@
 import pytest
 from src.string_calculater import string_calculater
 
-@pytest.mark.parametrize('n,expected_result', [('',0), ('0',0),('1',1),('2',2),('3',3),('0,0',0),('0,1',1),('1,0',1),('1,1',2),('1,2',3)])
+@pytest.mark.parametrize('n,expected_result', [('',0), ('0',0),('1',1),('2',2),('3',3),('0,0',0),('0,1',1),('1,0',1),('1,1',2),('1,2',3),('100',100),('1999',1999),('200,39',239),('2000,25',2025),('90,10',100),('40,5',45),('33,5',38),('67,3',70)])
 def test_n_returns_expected_result(n, expected_result):
     actual_result = string_calculater(n)
     assert actual_result == expected_result
