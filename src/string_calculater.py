e75f2099fa989a6c7d91b6a431c07a08f9602c93
Radja <radjaguemoula@gmail.com>
2025-10-29 23:00:29 +0100

    test 2 return 2

--- src/string_calculater.py
+++ src/string_calculater.py
@@ -1,4 +1,6 @@
 def string_calculater(n:str)-> int:
     if n == '1':
         return 1
+    if n == '2':
+        return 2
     return 0
--- tests/test_string_calculater.py
+++ tests/test_string_calculater.py
@@ -1,7 +1,7 @@
 import pytest
 from src.string_calculater import string_calculater
 
-@pytest.mark.parametrize('n,expected_result', [('',0), ('0',0),('1',1)])
+@pytest.mark.parametrize('n,expected_result', [('',0), ('0',0),('1',1),('2',2)])
 def test_n_returns_expected_result(n, expected_result):
     actual_result = string_calculater(n)
     assert actual_result == expected_result
